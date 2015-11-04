7cf7f05e81da8d9aa2e89cbf5658dc1a43a3bf26
mvnnn <mavanibhautik@gmail.com>
2015-11-05 01:07:59 +0530

    modify file:

--- cyberoam.py
+++ cyberoam.py
@@ -2,11 +2,14 @@
 import sys
 from urllib import urlencode
 from urllib2 import urlopen
+import getpass
+import urllib2
 from time import sleep
 from os import system
 from bs4 import BeautifulSoup
 import subprocess
 import pynotify
+import cookielib
 
 ''''' this cyberoam working on DA-IICT because i use DA IP
       even if you close terminal but process will running'''''
@@ -14,7 +17,14 @@ import pynotify
 
 cyberoamIP = "10.100.56.55"   #The IP of the Cyberoam site of DA-IICT,
 cyberoamPort = "8090"   #PORT number
+
+
+
 neverQuit=True
+
+
+
+
 sleeptime=10
 pynotify.init("Cyberoam")
 URL = 'https://'+cyberoamIP+":"+cyberoamPort+'/'+'httpclient.html'
@@ -22,8 +32,34 @@ URL = 'https://'+cyberoamIP+":"+cyberoamPort+'/'+'httpclient.html'
 
 n = pynotify.Notification("Cyberoam Login with valid DA_id and password")
 n.show()
+
+
+
 DA_id = raw_input("Enter your ID: ")
-passwordd = raw_input("password: ")
+#passwordd = raw_input("password: ")
+passwordd=getpass.getpass('Password:')
+
+
+
+
+def totalnetused(user,passs):
+    p=subprocess.Popen('ps', stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+    print p.communicate()
+    params = urlencode({'mode': 1, 'username': user, 'password': passs})
+    res = urlopen(URL, params)
+    if res != '':
+     bs = BeautifulSoup(res)
+     data= bs.html.body
+     data_info= []
+     for dt in data.readline():
+        start = dt.find('')
+        the_page = dt[start+53:]
+        end = the_page.find('data_info.append(dt[0:end])')
+     print data_info
+    else :
+     print "did not find data used"
+
+
 
 
 def forprocide():
@@ -42,14 +78,17 @@ def forprocide():
         ss=1
     return ss
 
+
+
 def Timerforlogin():
     sleep(sleeptime)
+
 
 def forLogin(user,passs):
         params = urlencode({'mode': 191, 'username': user, 'password': passs})
         res = urlopen(URL, params)
         aa=res.read()
-        print "login success"
+        print "login procedure"
         return aa
 
 
@@ -57,7 +96,7 @@ def forLogout(user):
         params = urlencode({'mode': 193, 'username': user})
         res = urlopen(URL, params)
         aa=res.read()
-        print "logout success"
+        print "logout procedure"
         return aa
 
 
@@ -66,32 +105,43 @@ def logout():
         if 'off' in info:
             n = pynotify.Notification("logout")
             n.show()
-            exit()
+        sys.exit()
 
 
 def loginLogout():
+
     relogin=False
+
     try:
         if "login" in sys.argv:
             info=forLogin(DA_id,passwordd)
             if "could not" in info:
-                n = pynotify.Notification("enter valid DA_id and password and run file")
+                n = pynotify.Notification("id or password incorrect...enter valid DA_id and password and run file")
                 n.show()
                 exit()
+
+
             elif "Maximum" in info:
                 n = pynotify.Notification("Maximum data limit")
                 n.show()
                 exit()
+
+
             elif "exceeded" in info:
                 n = pynotify.Notification("data limit exceeded")
                 n.show()
                 exit()
+
+
             elif "successfully logged in" in info :
                 n = pynotify.Notification("login success")
                 n.show()
                 relogin=True
                 print "Press Ctrl + C to logout"
+               # totalnetused(DA_id,passwordd)
                 #print forprocide()
+
+
 
             else:
                 print "this Cyberoam don't recognise your error..please try again"
@@ -112,25 +162,29 @@ def loginLogout():
                  sys.argv="login"
                  loginLogout()
 
+
+
                 else :
                     logout()
+                    totalnetused(DA_id,passwordd)
                     exit()
 
 
+        if "logout" in sys.argv:
+            logout()
+            exit()
 
 
     except KeyboardInterrupt:
         logout()
+        totalnetused(DA_id,passwordd)
         exit()
+
+
     except Exception as e:
         print e
 
-    try :
-        if "logout" in sys.argv:
-            logout()
-            exit()
-    except Exception as e:
-        print e
+
 
 
 if __name__ == "__main__":
